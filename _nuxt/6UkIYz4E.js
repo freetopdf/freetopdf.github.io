683be43f9e8794c692b57573f0655dd02d5359d4
DeuklyoungKo <lunaman1@naver.com>
2025-10-13 12:36:06 +0900

    update

---
+++ _nuxt/6UkIYz4E.js
@@ -0,0 +1,19 @@
+import { A as useHead$1, B as tryUseNuxtApp, C as hasInjectionContext, D as inject, E as headSymbol } from "./rJqLcTGV.js";
+function injectHead(nuxtApp) {
+  var _a;
+  const nuxt = nuxtApp || tryUseNuxtApp();
+  return ((_a = nuxt == null ? void 0 : nuxt.ssrContext) == null ? void 0 : _a.head) || (nuxt == null ? void 0 : nuxt.runWithContext(() => {
+    if (hasInjectionContext()) {
+      return inject(headSymbol);
+    }
+  }));
+}
+function useHead(input, options = {}) {
+  const head = injectHead(options.nuxt);
+  if (head) {
+    return useHead$1(input, { head, ...options });
+  }
+}
+export {
+  useHead as u
+};
